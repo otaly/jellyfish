b8d308680ac8a63985d266761253130d2568025c
otaly <60144056+otaly@users.noreply.github.com>
2024-07-15 05:56:51 +0900

    クエリパラメータを受け取る形に変更

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,15 +1,35 @@
-use axum::{routing::get, Router};
+use axum::{extract::Query, http::StatusCode, routing::get, Json, Router};
+use serde::{Deserialize, Serialize};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(hello));
+    let app = Router::new().route("/hello", get(hello));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:4000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn hello() -> &'static str {
-    "Hello, World!"
+async fn hello(params: Query<Params>) -> (StatusCode, Json<Greeting>) {
+    let Query(params) = params;
+    (
+        StatusCode::OK,
+        Json(Greeting {
+            greeting: match params {
+                Params { name: Some(name) } => format!("Hello, World! {}!!", name).into(),
+                Params { name: None } => "Hello, World!".into(),
+            },
+        }),
+    )
+}
+
+#[derive(Debug, Deserialize, Default)]
+struct Params {
+    name: Option<String>,
+}
+
+#[derive(Serialize)]
+struct Greeting {
+    greeting: String,
 }
 
 #[cfg(test)]
@@ -18,6 +38,6 @@ mod tests {
 
     #[tokio::test]
     async fn test_hello() {
-        assert_eq!(hello().await, "Hello, World!");
+        assert_eq!(hello(Query(Params { name: None })).await.0, StatusCode::OK);
     }
 }
