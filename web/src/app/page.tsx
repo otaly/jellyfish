17ef2eb03b79cdc9cca9d852acc9ed36e8c86d99
otaly <60144056+otaly@users.noreply.github.com>
2024-07-21 02:01:23 +0900

    Webのデプロイ時に環境変数API_URLを渡すよう変更

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,27 @@
-export default function Home() {
+import { API_URL } from './_lib/config';
+
+type Props = {
+  searchParams: {
+    name?: string;
+  };
+};
+
+const fetchGreeting = async (
+  params: Props['searchParams'],
+): Promise<{ greeting: string }> => {
+  const url = new URL('/hello', API_URL);
+  if (params.name) url.searchParams.append('name', params.name);
+
+  return (await fetch(url, { cache: 'no-store' })).json();
+};
+
+const Home = async ({ searchParams }: Props) => {
+  const { greeting } = await fetchGreeting(searchParams);
   return (
     <main className="flex min-h-dvh flex-col items-center p-16">
-      <h1>Hello, World!</h1>
+      <h1>{greeting}</h1>
     </main>
   );
-}
+};
+
+export default Home;
